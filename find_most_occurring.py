450615371a6566a4b2dd66795ce9033db7cf1371
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-08-12 16:32:33 +0100

    returns the most occuring character

--- find_most_occurring.py
+++ find_most_occurring.py
@@ -1,9 +1,10 @@
-sentence = "This is a common interview question"
+sentence = input(
+    "Input your sentence, let's find the most occurring character: ")
 
 
 def sort_char_by_occurence(string):
     clear_whitespaces = string.replace(" ", "").lower()
-    print(clear_whitespaces)
+    # print(clear_whitespaces)
     sorted_character = {}
     for char in clear_whitespaces:
         if char in sorted_character:
@@ -18,12 +19,16 @@ def get_most_occurring_char():
     sorted_char = sort_char_by_occurence(sentence)
     frequency = []
     for item in sorted_char:
-        print(item, "> length: ", len(sorted_char[item]))
-        frequency.append(sorted_char[item])
+        frequency.append(len(sorted_char[item]))
+    sorted_freq = sorted(frequency)
 
-    print(frequency)
+    # print(sorted_freq)
 
-    return sorted_char
+    for item in sorted_char:
+        if len(sorted_char[item]) == sorted_freq[-1]:
+            return f"\n=> The most occurring character in: '{sentence}' is \"{item}\"\n"
+
+    # return sorted_char
 
 
 print(get_most_occurring_char())
